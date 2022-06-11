0943cfe4734d351f2ace8d631bd95b661e9a1f59
bobby <ronanlai.hk@gmail.com>
2022-06-10 21:21:09 -0500

    "test"

--- getdata.py
+++ getdata.py
@@ -1,7 +1,8 @@
-from click import option
-from matplotlib.pyplot import xlim
 import requests
 from bs4 import BeautifulSoup
+from datetime import date
+import time
+import json
 
 #Biden 538 approval rating June 15th
 which_market = "7973"                           #str(input("ID for market? : "))
@@ -40,7 +41,7 @@ def option_names(dataset=use):
         x+=1
     return(names)
 
-#Returns dictonary with name, yes value, and no value
+#Returns list with name of row and price values
 def names_and_values(yes_buy, no_buy, yes_sell, no_sell, dataset=use):
     data = []
     x = 2
@@ -50,15 +51,42 @@ def names_and_values(yes_buy, no_buy, yes_sell, no_sell, dataset=use):
         row_no_buy = get_value(x, no_buy, dataset)[1]
         row_yes_sell = get_value(x, yes_sell, dataset)[1]
         row_no_sell = get_value(x, no_sell, dataset)[1]
-        whole = {"Name": row_name, "Values": {"Yes Buy Value": row_yes_buy, "No Buy Value": row_no_buy, "Yes Sell Value": row_yes_sell, "No Sell Value": row_no_sell, }}
+        #whole = {"Name": row_name, "Values": {"Yes Buy Value": row_yes_buy, "No Buy Value": row_no_buy, "Yes Sell Value": row_yes_sell, "No Sell Value": row_no_sell, }}
+        whole = [row_name, [row_yes_buy, row_no_buy, row_yes_sell, row_no_sell]]
         data.append(whole)
         x+=1
     return(data)
 
-import json
+def get_date():
+    return str(date.today())
+
+def get_time():
+    return str(time.strftime("%H:%M:%S"))
+
 to_insert = {
-"Title": get_title(), 
+get_title(): {
+"Date": get_date(),
+"Time": get_time(),
 "Contents": names_and_values(7,8,9,10)
 }
-with open('saved.json', 'w+') as json_data:
-   json.dump(to_insert, json_data)
+}
+
+def write_json(new_data, filename='saved.json'):
+    with open(filename, 'w') as file:
+        json.dump(new_data, file, indent=4)
+
+blah = {
+"Date": 33,
+"Time": 44,
+"Contents": 44
+}
+
+with open('saved.json') as json_file:
+    data = json.load(json_file)
+    temp = data["What will Joe Biden's 538 job approval rating be for June 15?"]
+    temp.append(blah)
+
+write_json(data)
+
+#with open('saved.json', 'w+') as json_data:
+#   json.dump(to_insert, json_data)

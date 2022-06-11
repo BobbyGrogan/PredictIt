e4daff33b75a158321298c3c315fd2f9b1dbb8d8
bobby <ronanlai.hk@gmail.com>
2022-06-11 12:29:22 -0500

    CSV

--- getdata.py
+++ getdata.py
@@ -12,13 +12,15 @@ def get_api(which_market="7956"):
     this = str(soup).split('{')
     return this
 
+use = get_api()
+
 #Returns title of subject
-def get_title(dataset=get_api()):
+def get_title(dataset=use):
     title = (dataset[1].split(",")[1]).split('"')[3]
     return(title)
 
 #Returns individual value
-def get_value(row, cell, dataset=get_api()):
+def get_value(row, cell, dataset=use):
     got = (dataset[row]).split(",")[cell]
     cell_name = got.split(":")[0].replace("\r\n","").replace(" ","").replace('"',"")
     cell_value = got.split(":")[1].replace(" ","").replace('"','')
@@ -26,13 +28,13 @@ def get_value(row, cell, dataset=get_api()):
     return(both)
 
 #Returns the title and number of betting options
-def overall(dataset=get_api()):
+def overall(dataset=use):
     length = len(dataset) - 2
     all = [get_title(dataset), str(length) + " Options"]
     return(all)
 
 #Returns the names of the various options
-def option_names(dataset=get_api()):
+def option_names(dataset=use):
     names = []
     x = 2
     while len(dataset) > x:
@@ -49,15 +51,15 @@ def get_time():
     return str(time.strftime("%H:%M:%S"))
 
 #Returns list with values for rows in CSV
-def get_values(yes_buy=7, no_buy=8, yes_sell=9, no_sell=10, dataset=get_api()):
+def get_values(dataset=use):
     values = [get_title(), get_date(), get_time()]
     x = 2
     while len(dataset) > x:
         row_name = get_value(x, 3, dataset)[1]
-        row_yes_buy = get_value(x, yes_buy, dataset)[1]
-        row_no_buy = get_value(x, no_buy, dataset)[1]
-        row_yes_sell = get_value(x, yes_sell, dataset)[1]
-        row_no_sell = get_value(x, no_sell, dataset)[1]
+        row_yes_buy = get_value(x, 7, dataset)[1]
+        row_no_buy = get_value(x, 8, dataset)[1]
+        row_yes_sell = get_value(x, 9, dataset)[1]
+        row_no_sell = get_value(x, 10, dataset)[1]
         whole = [row_yes_buy, row_no_buy, row_yes_sell, row_no_sell]
         for n in whole:
             values.append(n)
@@ -65,7 +67,7 @@ def get_values(yes_buy=7, no_buy=8, yes_sell=9, no_sell=10, dataset=get_api()):
     return(values)
 
 #Returns a list with values for column names for CSV
-def get_fields(dataset=get_api()):
+def get_fields(dataset=use):
     fields = ["Title", "Date", "Time"]
     x = 2
     while len(dataset) > x:
@@ -77,7 +79,6 @@ def get_fields(dataset=get_api()):
     return(fields)
 
 fields = get_fields()
-values = get_values()
 filename = "7956.csv"
 
 def format_csv():
@@ -85,16 +86,17 @@ def format_csv():
         csvwriter = csv.writer(csvfile)
         csvwriter.writerow(fields)
 
-def update_csv():
+def update_csv(recent):
     with open(filename, "a", newline="") as f:
         writer_object = writer(f)
-        writer_object.writerow(values)
+        writer_object.writerow(get_values(recent))
         f.close()
 
 def repeat_update():
     x = 1
     while x ==1:
-        update_csv()
+        current = get_api()
+        update_csv(current)
         print("Updated")
         time.sleep(80)
 
